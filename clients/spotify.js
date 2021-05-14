ee982941b267bddd8318272348262bb590549de4
note072 <brunocordioli072@gmail.com>
2021-05-13 22:09:25 -0300

    removed spotify credentials

--- clients/spotify.js
+++ clients/spotify.js
@@ -4,8 +4,6 @@ const vuexLocal = localStorage.getItem('vuex');
 const vuex = JSON.parse(vuexLocal);
 
 export const spotify = new SpotifyWebApi({
-  clientId: process.env.SPOTIFY_CLIENT_ID,
-  clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
   accessToken:
     vuex && vuex.client && vuex.client.access_token ?
       vuex.client.access_token :
